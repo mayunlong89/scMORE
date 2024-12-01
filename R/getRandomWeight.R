71e53bc56ceb9115d29028c6fc0be8dacbb37fb6
Yunlong Ma <11418032@zju.edu.cn>
2024-12-01 04:35:39 -0500

    add filter

--- R/getRandomWeight.R
+++ R/getRandomWeight.R
@@ -4,6 +4,7 @@
 #' @param regulons regulon matrix from 'snp2peak_map'
 #' @param random_scores genes random Zscore and specificity score
 #' @return The function gives random importance weights for background genes
+#' @import fitdistrplus
 #' @export
 #'
 getRandomWeight <- function(regulons, random_scores) {
@@ -12,8 +13,11 @@ getRandomWeight <- function(regulons, random_scores) {
   importance_weights <- regulons$Importance_weighted
   background_genes <- random_scores$genes
 
+  # filter NA and 0 value
+  importance_weights <- importance_weights[!is.na(importance_weights) & importance_weights > 0]
+
   # Fit a gamma distribution to importance weights
-  fit_gamma <- fitdist(importance_weights, "gamma")
+  fit_gamma <- fitdistrplus::fitdist(importance_weights, "gamma")
 
   # Generate random importance weights directly from the gamma distribution
   random_importance_weights <- rgamma(
