7b16e21466127f836e49193b1e37f0cf0a7ea3a0
Yunlong Ma <11418032@zju.edu.cn>
2024-12-27 15:22:31 -0500

    Update regulon2disease.R

--- R/regulon2disease.R
+++ R/regulon2disease.R
@@ -22,8 +22,8 @@ regulon2disease <- function(grn_outputs,
                             top_n = 5,
                             buffer = 500) {
 
-  # Step 1: Map SNPs to TF-peaks and target genes
-  message("Step 1: Mapping SNPs to TF-peaks and target genes...")
+  # Step 4.1: Map SNPs to TF-peaks and target genes
+  message("Step 4.1: Mapping SNPs to TF-peaks and target genes...")
   peak2gene_strength <- peak2gene(grn_outputs)  # Map peaks to genes
   snp2peak_map <- snp2peak(snp_info, peak2gene_strength, buffer = buffer)  # SNP-to-peak mapping
 
@@ -34,8 +34,8 @@ regulon2disease <- function(grn_outputs,
   # Extract regulons containing SNP, peak, TF, target gene, and importance score
   regulons <- snp2peak_map[, c("snp_id", "peak_ids", "TF", "Target", "Importance_weighted")]
 
-  # Step 2: Define TF list and cell types
-  message("Step 2: Defining TF list and cell types...")
+  # Step 4.2: Define Regulon list and cell types
+  message("Step 4.2: Defining Regulon list and cell types...")
   tf_list <- grn_outputs$tf_names  # Transcription factors
   all_celltype_names <- unique(target_scores[, "celltypes"])  # Cell types
 
@@ -59,7 +59,8 @@ regulon2disease <- function(grn_outputs,
   total_run <- length(all_celltype_names) * length(tf_list)
   count <- 0
 
-  # Step 3: Perform COSR and MORE analysis for each cell type and TF
+  # Step 4.3: Perform scMORE analysis for each cell type and regulon
+  message("Step 4.3: Calculate Trait-Asssociated Regulon Score and Signifiance...")
   for (i in seq_along(all_celltype_names)) {
     for (j in seq_along(tf_list)) {
 
@@ -152,7 +153,8 @@ regulon2disease <- function(grn_outputs,
   end_time <- Sys.time()
   cat("Running time:", round(difftime(end_time, start_time, units = "secs"), 2), "seconds\n")
 
-  # Add significance column
+  # Step 4.4 Add significance column
+  message("Step 4.4: Adding Significance column...")
   all_regulon_results_df$Significance <- ifelse(
     all_regulon_results_df$SpecificityScore_p < 0.05 &
       all_regulon_results_df$ImportanceWeightScore_p < 0.05 &
