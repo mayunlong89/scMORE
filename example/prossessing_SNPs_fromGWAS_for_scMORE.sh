af591ef16e39d5b289e3d6c70febdb58e7da5118
Yunlong Ma <11418032@zju.edu.cn>
2025-01-13 17:32:49 -0500

    Update prossessing_SNPs_fromGWAS_for_scMORE.sh

--- example/prossessing_SNPs_fromGWAS_for_scMORE.sh
+++ example/prossessing_SNPs_fromGWAS_for_scMORE.sh
@@ -1,3 +1,7 @@
+
+#### Processing SNPs from GWAS summary statistics to form the 'snp_info' file
+
+```bash
 ##--- Contains 6 columns: ES:SE:LP:AF:SS:ID
 cat ieu-b-31.vcf | grep -v "#" | cut -f 1,2,10 | sed 's/:/\t/g' | awk '$6>=0.01' > monocyte_count_maf0.01.txt
 
@@ -10,3 +14,13 @@ cat ebi-a-GCST004627.vcf | grep -v "#" | cut -f 1,2,10 | sed 's/:/\t/g' | awk '$
 ##--- We used SNP MAF data from PD GWAS summary statistics (from European ancestry samples with a larger sample size) as a reference.
 
 awk 'NR==FNR {maf[$1]=$2; next} ($1 in maf) && maf[$1]>=0.01' ../02_PD/PD_gwas_maf.txt AD_maf.txt > AD_filtered_maf0.01.txt
+
+## for frailty index GWAS------
+#version 1
+awk '$6 >= 0.01 {print $2, $3, $7, $8, -log($9)/log(10), $6, $1}' GCST90020053_GRCh37.tsv > frailty_index_maf0.01.txt
+
+#version 2
+cat GCST90020053_GRCh37.tsv | awk '$6 >= 0.01' | awk '{print $2, $3, $7, $8, -log($9)/log(10), $6, $1}'  > frailty_index_maf0.01.txt
+
+#change the header into: CHR, POS, ES, SE, LP, AF, SNP
+```
