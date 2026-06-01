bba7e6bb01c2894774369c55c4ad913d88363d6a
Yunlong Ma <11418032@zju.edu.cn>
2026-06-01 12:28:50 -0400

    Update peak2gene.R

--- R/peak2gene.R
+++ R/peak2gene.R
@@ -45,7 +45,7 @@ peak2gene <- function(grn_outputs, infer_method = 'glm') {
     dplyr::mutate(
       Strength = switch(
         infer_method,
-        glm = -log10(Pval+1e-323),
+        glm = pmin(-log10(pmax(Pval, .Machine$double.xmin)), 300),
         cv.glmnet = (Corr + 1) / 2,
         glmnet = (Corr + 1) / 2,
         xgb = Gain + 0.00005
